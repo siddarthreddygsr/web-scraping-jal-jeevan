f5213d656749410e0f732aa774ccc85d201cac50
Siddarthreddygsr <siddarthreddygsr@gmail.com>
2024-03-11 07:33:25 +0530

    scraping 2 tables

--- myneta/candidate_scraper.py
+++ myneta/candidate_scraper.py
@@ -3,9 +3,40 @@ import requests
 import re
 import pdb
 import json
+from pprint import pprint
 
-def parse(url):
-    response = requests.get(url)
+proxies = {
+        'http':"http://20.111.54.16:8123"
+    }
+
+def cases_table(soup):
+    try:
+        cases_table_rows = soup.find('table', attrs={'id':'cases'}).find_all('tr')
+        cases_meta_data = [cell.text for cell in cases_table_rows[0].find_all('td')]
+        cases_json_data = []
+        for row in cases_table_rows[1:]:
+            row_data = {}
+            flag = 0
+            edge_cases = 0
+            for cell in row.find_all('td'):
+                try:
+                    row_data[cases_meta_data[flag]] = cell.text.replace('\xa0', ' ')
+                    flag += 1
+                except:
+                    edge_cases += 1
+            if bool(row_data):
+                cases_json_data.append(row_data)
+        cases_data = json.dumps(cases_json_data)
+    except:
+        cases_data = "No data available on site"
+    return cases_data
+
+
+def parse(url, proxy_flag):
+    if proxy_flag:
+        response = requests.get(url, proxies=proxies)
+    else:
+        response = requests.get(url)
     
     soup = BeautifulSoup(response.text, 'html.parser')
     cand_dict = {}
@@ -21,7 +52,19 @@ def parse(url):
         cand_dict["age"] = w3_panel.find_all("div")[4].get_text(strip=True).replace('Age:', '').strip()
         cand_dict["party"] = w3_panel.find_all('div')[1].find('div').get_text(strip=True).replace('Part:','').split(":")[-1]
         cand_dict["url"] = url
-        cand_dict["assets"], cand_dict["liabilities"] = [value.get_text(strip = True).replace('\xa0', ' ') for value in soup.find_all(class_="w3-table")[0].find_all('b')]
+        it_table_rows = soup.find('table', attrs={'id':'income_tax'}).find_all('tr')
+        meta_data = [cell.text for cell in it_table_rows[0].find_all('th')]
+        json_data = []
+        for row in it_table_rows:
+            row_data = {}
+            flag = 0
+            for cell in row.find_all('td'):
+                row_data[meta_data[flag]] = cell.text.replace('\xa0', ' ')
+                flag += 1
+            if bool(row_data):
+                json_data.append(row_data)
+        cand_dict["income_tax_data"] = json.dumps(json_data)
+        cand_dict["cases_data"] = cases_table(soup)
         cand_dict["self_prof"] = w3_panel.find('p').find('b', string='Self Profession:').next_sibling.strip()
         cand_dict["spouse_prof"] = w3_panel.find('p').find('b', string='Spouse Profession:').next_sibling.strip().replace("\n","")
         w3_red = soup.find_all(class_ = "w3-red")
@@ -30,27 +73,12 @@ def parse(url):
         else:
             cand_dict["criminal_cases"] = soup.find_all(class_ = "w3-red")[0].find("span").get_text(strip = True)
         cand_dict["education"] = soup.find_all(class_ = "w3-panel")[4].find_all(class_ = "w3-panel")[2].get_text(strip = True).replace("Educational DetailsCategory:","").replace(",",".").strip()
+        cand_dict["assets"], cand_dict["liabilities"] = [value.get_text(strip = True).replace('\xa0', ' ') for value in soup.find_all(class_="w3-table")[0].find_all('b')]
     except:
         print(f"{url}\n{url}\n{url}\n{url}\n{url}\n")
     return cand_dict
 # print(parse("https://myneta.info/telangana2023/candidate.php?candidate_id=309"))
 
-def table1(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    table_rows = soup.find('table', attrs={'id':'income_tax'}).find_all('tr')
-    meta_data = [cell.text for cell in table_rows[0].find_all('th')]
-    json_data = []
-    for row in table_rows:
-        row_data = {}
-        flag = 0
-        for cell in row.find_all('td'):
-            row_data[meta_data[flag]] = cell.text.replace('\xa0', ' ')
-            flag += 1
-        if bool(row_data):
-            json_data.append(row_data)
-    income_tax_data = json.dumps(json_data)
-    return income_tax_data
 
 
-print(table1("https://myneta.info/telangana2023/candidate.php?candidate_id=309"))
+# pprint(table1("https://myneta.info/telangana2023/candidate.php?candidate_id=453"))
--- myneta/main.py
+++ myneta/main.py
@@ -3,14 +3,39 @@ from bs4 import BeautifulSoup
 from candidate_scraper import parse
 import pandas as pd
 from tqdm import tqdm
+import argparse
+import uuid
 
 dataframe = []
 candidate_urls_main =  []
 progress_bar = tqdm(total=50, position=0, desc="Processing Districts", unit="district", dynamic_ncols=True, ascii=" ▖▘▝▗▚▞█", colour="GREEN")
-dist_cnt = 1
+dist_cnt = 100
 progress_made = 0
+prompt = """
+ ___      ___  ___  ___      _____  ___    _______  ___________   __      
+|"  \    /"  ||"  \/"  |    (\"   \|"  \  /"     "|("     _   ") /""\     
+ \   \  //   | \   \  /     |.\\   \    |(: ______) )__/  \\__/ /    \    
+ /\\  \/.    |  \\  \/      |: \.   \\  | \/    |      \\_ /   /' /\  \   
+|: \.        |  /   /       |.  \    \. | // ___)_     |.  |  //  __'  \  
+|.  \    /:  | /   /        |    \    \ |(:      "|    \:  | /   /  \\  \ 
+|___|\__/|___||___/          \___|\____\) \_______)     \__|(___/    \___)
+
+SUGGESTION: USE -p FLAG TO USE PROXY SERVERS
+                                                                          
+"""
+print(prompt)
+parser = argparse.ArgumentParser(description="")
+parser.add_argument('-p', '--proxy', action='store_true',help='Flag to use proxy')
+args = parser.parse_args()
+proxies = {
+        'http':"http://20.111.54.16:8123"
+    }
+print(f"[{str(uuid.uuid4().hex)[:8]}] Using Proxy: {proxies["http"]}") if args.proxy else print(f"[{str(uuid.uuid4().hex)[:8]}] Proxies disabled")
 for district in range(dist_cnt):
-    response = requests.get(f"https://myneta.info/telangana2023/index.php?action=show_constituencies&state_id={district+1}")
+    if args.proxy:
+        response = requests.get(f"https://myneta.info/telangana2023/index.php?action=show_constituencies&state_id={district+1}", proxies=proxies)
+    else:
+        response = requests.get(f"https://myneta.info/telangana2023/index.php?action=show_constituencies&state_id={district+1}")
     soup = BeautifulSoup(response.text, 'html.parser')
 
     candidate_links = soup.find_all('a', href=lambda href: href and 'candidate.php?candidate_id=' in href)
@@ -29,7 +54,7 @@ for district in range(dist_cnt):
 progress_bar.close()
 
 for candidate_url in tqdm(candidate_urls_main, desc="Processing candidates", unit="candidate",  ascii=" ▖▘▝▗▚▞█", colour="RED"):
-    parsed_data = parse(url=candidate_url)
+    parsed_data = parse(url=candidate_url, proxy_flag = args.proxy)
     print(parsed_data)
     dataframe.append(parsed_data)
 
